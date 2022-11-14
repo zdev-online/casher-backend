172633ac83145253c810aa04662344c0a02dc214
zdev-online <23assasincred03@gmail.com>
2022-11-15 01:14:31 +0300

    set a scope of token module to global

--- src/token/token.module.ts
+++ src/token/token.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TokenService } from './token.service';
 
+@Global()
 @Module({
 	imports: [JwtModule.register({})],
 	providers: [TokenService],
