b90521016628c79f8d9796a8e44c3986d6fb94b7
zdev-online <23assasincred03@gmail.com>
2022-11-15 06:02:05 +0300

    add default gateway options

---
+++ src/common/constants/default-gateway-options.constants.ts
@@ -0,0 +1,6 @@
+export const defaultGatewayOptions = {
+	cors: {
+		origin: '*',
+		credentials: true,
+	},
+};
--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -3,3 +3,4 @@ export * from './constraints.constants';
 export * from './keys.constants';
 export * from './socket-events.contants';
 export * from './socket-namespaces.constants';
+export * from './default-gateway-options.constants';
